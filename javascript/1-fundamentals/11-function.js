69ef2709e893d0262d721619956a54687cb58d28
jaganjavid <jagantutor1@gmail.com>
2024-04-03 08:11:33 +0530

    Added a function

---
+++ javascript/1-fundamentals/11-function.js
@@ -0,0 +1,41 @@
+
+
+// Two Phases
+
+// 1 - Creation
+// 2 - Exectation
+
+
+
+//  Function Decalaration - Function Statement
+
+// console.log(greet("Jagan", "Javid"));
+
+function greet(firstName = "John", lastName = "Doe"){
+    
+    // if(typeof firstName === "undefined"){
+    //     firstName = "John";
+    // }
+
+    // if(typeof lastName === "undefined"){
+    //     lastName = "Doe";
+    // }
+
+    return `${firstName} ${lastName}`;
+}
+
+
+// console.log(greet("Jagan", "Javid"));
+
+
+// Function Expression
+
+// add(5,5);
+
+var add = function(x, y){
+  console.log(x + y);
+}
+
+
+add(5,5);
+
