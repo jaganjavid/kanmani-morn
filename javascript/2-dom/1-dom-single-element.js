761291de6ee5471fa9ac724b4d08b5313af3ec82
jaganjavid <jagantutor1@gmail.com>
2024-04-04 08:13:28 +0530

    started a DOM

---
+++ javascript/2-dom/1-dom-single-element.js
@@ -0,0 +1,20 @@
+
+
+// document.getElementById();
+
+
+// console.log(document.getElementById("task-title"));
+
+const taskTitle = document.getElementById("task-title");
+
+// console.log(taskTitle);
+
+// Change Style
+
+taskTitle.style.backgroundColor = "blue";
+taskTitle.style.color = "#ffffff";
+taskTitle.style.padding = "15px";
+
+
+// Change Content
+taskTitle.innerText = "Hello";
---
+++ javascript/2-dom/app.js
@@ -0,0 +1,20 @@
+
+
+let val;
+
+
+val = document;
+val = document.all;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+
+val = document.forms;
+val = document.forms[0].id;
+
+val = document.links;
+val = document.all[6];
+
+
+console.log(val);
